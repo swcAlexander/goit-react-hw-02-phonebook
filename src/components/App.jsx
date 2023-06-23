cf3220d1c6176f4c380feba067328bbf2b30d4a6
Alexander Klymuk <swcAlexander@gmail.com>
2023-06-23 15:35:19 +0300

    add start files and some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import {Container} from "components/Container/Container"
+
+
+
+
+export class App extends React.Component {
+    static propTypes = {
+        // тут описуємо пропи
+    }
+    // state = {
+    //     value: this.props.initialValue,
+    // }
+    state = {
+      contacts: [],
+      name: ''
+    }
+
+
+    render() {
+        
+      return (
+        <Container>
+          <input
+              type="text"
+              value={this.state.name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          
+          </Container>
+            
+        )
+    }
+
+}
